cc92a704ca195a3eccbc4a338f891048e671f35b
sarvesh-karki <sarveshkarki93@gmail.com>
2023-09-28 12:24:48 +0545

    login page update

--- apps/admin/src/components/web3/ConnectWithSelect.tsx
+++ apps/admin/src/components/web3/ConnectWithSelect.tsx
@@ -1,12 +1,15 @@
-import { Web3ReactHooks } from '@web3-react/core';
-import { GnosisSafe } from '@web3-react/gnosis-safe';
-import type { MetaMask } from '@web3-react/metamask';
-import { Network } from '@web3-react/network';
-import { useCallback, useEffect, useState } from 'react';
+import { Web3ReactHooks } from "@web3-react/core";
+import { GnosisSafe } from "@web3-react/gnosis-safe";
+import type { MetaMask } from "@web3-react/metamask";
+import { Network } from "@web3-react/network";
+import { useCallback, useEffect, useState } from "react";
 
-import { Button } from '@mui/material';
-import { sign } from './utils/wallet';
+import { Button } from "@mui/material";
+import { sign } from "./utils/wallet";
 import { JsonRpcProvider, Signer } from "ethers";
+import { useLoginWallet, useNonceGet } from "@hooks/web3/useMetamask";
+import { useRouter } from "next/router";
+import { saveAccessToken, saveCurrentUser } from "@utils/sessionManager";
 
 function ChainSelect({
   activeChainId,
@@ -16,8 +19,15 @@ function ChainSelect({
   connectWallet: () => void;
 }) {
   return (
-    <Button variant="contained" value={activeChainId} onClick={(e) => { connectWallet() }}>Connect Metamask</Button>
-
+    <Button
+      variant="contained"
+      value={activeChainId}
+      onClick={(e) => {
+        connectWallet();
+      }}
+    >
+      Connect Metamask
+    </Button>
   );
 }
 
@@ -31,39 +41,72 @@ export function ConnectWithSelect({
   setError,
 }: {
   connector: MetaMask | Network | GnosisSafe;
-  activeChainId: ReturnType<Web3ReactHooks['useChainId']>;
-  chainIds?: ReturnType<Web3ReactHooks['useChainId']>[];
-  isActivating: ReturnType<Web3ReactHooks['useIsActivating']>;
-  isActive: ReturnType<Web3ReactHooks['useIsActive']>;
+  activeChainId: ReturnType<Web3ReactHooks["useChainId"]>;
+  chainIds?: ReturnType<Web3ReactHooks["useChainId"]>[];
+  isActivating: ReturnType<Web3ReactHooks["useIsActivating"]>;
+  isActive: ReturnType<Web3ReactHooks["useIsActive"]>;
   error: Error | undefined;
-  provider: ReturnType<Web3ReactHooks['useProvider']>
+  provider: ReturnType<Web3ReactHooks["useProvider"]>;
   setError: (error: Error | undefined) => void;
 }) {
-
   const [desiredChainId, setDesiredChainId] = useState<any>(undefined);
 
-  const getSignature = async () => {
-    try {
-      const signer = (provider as unknown as JsonRpcProvider).getSigner() as unknown as Signer;
-      const sig = await signer.signMessage("1234");
-      console.log({ signer, sig })
+  const { data: nonceData, isSuccess: isNonceSuccess } = useNonceGet();
 
-      console.log(sig)
+  const { push } = useRouter();
+
+  const {
+    mutate,
+    isError,
+    data: loginWalletData,
+    isSuccess: isLoginWalletSuccess,
+    error: loginWalletError,
+  } = useLoginWallet();
+
+  const getSignature = async () => {
+    if (isNonceSuccess) {
+      try {
+        const signer = (provider as unknown as JsonRpcProvider).getSigner() as unknown as Signer;
+        const address = await signer.getAddress();
+        signer
+          .signMessage(nonceData?.nonce)
+          .then((res) => {
+            const signature = `${nonceData?.nonce}:${res}`;
+            mutate({ walletAddress: address, signature });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } catch (e) {
+        console.log(e);
+      }
     }
-    catch (e) {
-      console.log(e)
+  };
+
+  useEffect(() => {
+    isError && console.log(loginWalletError);
+    console.log(loginWalletData);
+    if (isLoginWalletSuccess) {
+      const currentUser = {
+        email: loginWalletData.data.email,
+        username: loginWalletData.data.name,
+      };
+      saveCurrentUser(currentUser);
+      saveAccessToken(loginWalletData.data.access_token);
+
+      push("/dashboard");
     }
-  }
+  }, [isError, isLoginWalletSuccess]);
+
   useEffect(() => {
     if (activeChainId && (!desiredChainId || desiredChainId === -1)) {
       setDesiredChainId(activeChainId);
     }
-    console.log("sign metamask 2", isActive)
+    console.log("sign metamask 2", isActive);
     // if (isActive) {
     //   getSignature()
     // }
   }, [desiredChainId, activeChainId]);
-
 
   const connectWallet = useCallback(async () => {
     try {
@@ -71,35 +114,47 @@ export function ConnectWithSelect({
 
       await connector.activate();
 
-
-      console.log("sign metamask 1")
+      console.log("sign metamask 1");
     } catch (error) {
-      console.log("error", error)
+      console.log("error", error);
       setError(error);
     }
   }, [connector, setError]);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-
-      <div style={{ marginBottom: '1rem' }} />
+    <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+      <div style={{ marginBottom: "1rem" }} />
       {isActive ? (
         error ? (
-          <Button variant="contained" color='error' onClick={() => connectWallet()}>Try again?</Button>
-        ) : (
-          <Button variant="contained" color='error'
-            onClick={() => {
-              // if (connector?.deactivate) {
-              //   void connector.deactivate();
-              // } else {
-              //   void connector.resetState();
-              // }
-              // setDesiredChainId(undefined);
-              getSignature()
-            }}
-          >
-            Disconnect
+          <Button variant="contained" color="error" onClick={() => connectWallet()}>
+            Try again?
           </Button>
+        ) : (
+          <>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                getSignature();
+              }}
+            >
+              Login with metamask
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => {
+                if (connector?.deactivate) {
+                  void connector.deactivate();
+                } else {
+                  void connector.resetState();
+                }
+                setDesiredChainId(undefined);
+              }}
+            >
+              Disconnect
+            </Button>
+          </>
         )
       ) : (
         <ChainSelect activeChainId={desiredChainId} connectWallet={connectWallet} />
