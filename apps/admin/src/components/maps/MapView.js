d31cf541ab5bd0505b68a6f2e888fdcd476476a4
sarvesh-karki <sarveshkarki93@gmail.com>
2023-10-05 12:14:47 +0545

    mapping school location in maps

--- apps/admin/src/components/maps/MapView.js
+++ apps/admin/src/components/maps/MapView.js
@@ -1,35 +1,35 @@
 // import { MAPBOX_API } from "../";
-import styled from "@emotion/styled";
-import { useTheme } from "@mui/material";
-import dynamic from "next/dynamic";
+import styled from '@emotion/styled';
+import { useTheme } from '@mui/material';
+import dynamic from 'next/dynamic';
 
 // sections
-const MapClusters = dynamic(() => import("./ClusterMap"));
+const MapClusters = dynamic(() => import('./ClusterMap'));
 
 // ----------------------------------------------------------------------
 
 const THEMES = {
-  streets: "mapbox://styles/mapbox/streets-v11",
-  outdoors: "mapbox://styles/mapbox/outdoors-v11",
-  light: "mapbox://styles/mapbox/light-v10",
-  dark: "mapbox://styles/mapbox/dark-v10",
-  satellite: "mapbox://styles/mapbox/satellite-v9",
-  satelliteStreets: "mapbox://styles/mapbox/satellite-streets-v11",
+  streets: 'mapbox://styles/mapbox/streets-v11',
+  outdoors: 'mapbox://styles/mapbox/outdoors-v11',
+  light: 'mapbox://styles/mapbox/light-v10',
+  dark: 'mapbox://styles/mapbox/dark-v10',
+  satellite: 'mapbox://styles/mapbox/satellite-v9',
+  satelliteStreets: 'mapbox://styles/mapbox/satellite-streets-v11',
 };
 
 const baseSettings = {
   mapboxAccessToken:
-    "pk.eyJ1IjoicnVtc2FuIiwiYSI6ImNsN3pwc2ltajAzcjY0NHBtNzAycnI4dDMifQ.HhHwz1w0X4dleTgN4D-Kxw",
+    'pk.eyJ1IjoicnVtc2FuIiwiYSI6ImNsN3pwc2ltajAzcjY0NHBtNzAycnI4dDMifQ.HhHwz1w0X4dleTgN4D-Kxw',
   minZoom: 1,
-  scrollZoom: false,
+  scrollZoom: true,
 };
 
-const StyledMapContainer = styled("div")(({ theme, sx }) => {
+const StyledMapContainer = styled('div')(({ theme, sx }) => {
   return {
     zIndex: 0,
     height: 400,
-    overflow: "hidden",
-    position: "relative",
+    overflow: 'hidden',
+    position: 'relative',
     ...sx,
   };
 });
