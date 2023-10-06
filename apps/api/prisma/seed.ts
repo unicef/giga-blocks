80028dd152e5ba9a31fecff3f8085e2efc4711bd
anupamakoirala <anukoirala.96@hotmail.com>
2023-10-06 17:13:09 +0545

    fixes in seed

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,10 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/application';
 import * as _ from 'lodash';
-import { hexStringToBuffer } from '../src/utils/string-format';
+// import { hexStringToBuffer } from '../src/utils/string-format';
 
 const prisma = new PrismaClient();
 const logger = new Logger();
+
+const hexStringToBuffer = (str: string): Buffer => {
+  if (!str) return;
+  return Buffer.from(str.substring(2), 'hex');
+};
 export const roles = [
   {
     id: 1,
@@ -47,31 +52,31 @@ export const users = [
   {
     name: 'Sarvesh Admin',
     roles: [ROLE.ADMIN],
-    email: 'admin12@mailinator.com',
+    email: 'sarvesh@mailinator.com',
     walletAddress: hexStringToBuffer('0xCf7bb7da4eFFcA9a3974b80e106C6541990De5eb'), // add own wallet address
   },
   {
     name: 'Anupama Admin',
     roles: [ROLE.ADMIN],
-    email: 'admin123@mailinator.com',
+    email: 'anu123@mailinator.com',
     walletAddress: hexStringToBuffer('0xf0c84735Af5669c809EfD62C9D4e466d331A95b0'), // add own wallet address
   },
   {
     name: 'Manjik Admin',
     roles: [ROLE.ADMIN],
-    email: 'admin124@mailinator.com',
+    email: 'manjik@mailinator.com',
     walletAddress: hexStringToBuffer('0xcDEe632FB1Ba1B3156b36cc0bDabBfd821305e06'), // add own wallet address
   },
   {
     name: 'Sandesh Admin',
     roles: [ROLE.ADMIN],
-    email: 'admin125@mailinator.com',
+    email: 'sandesh@mailinator.com',
     walletAddress: hexStringToBuffer('0x6494650c466a166CBAC0BFeCD224286D6F03444F'), // add own wallet address
   },
   {
     name: 'Sushant Admin',
     roles: [ROLE.ADMIN],
-    email: 'admin126@mailinator.com',
+    email: 'sushant@mailinator.com',
     walletAddress: hexStringToBuffer('0xfAAe0B09e9A80d142A11cd846CD9329F2E96f55F'), // add own wallet address
   },
   {
