145eaaa9a074ea7377af903e70f6e0d5e019bb6b
sarvesh-karki <sarveshkarki93@gmail.com>
2023-10-06 14:25:07 +0545

    update

--- apps/admin/src/components/dashboard-cards/index.tsx
+++ apps/admin/src/components/dashboard-cards/index.tsx
@@ -1,22 +1,22 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
-import { useSchoolCount } from "@hooks/school/useSchool";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
+import { useSchoolCount } from '@hooks/school/useSchool';
 
 const bull = (
-  <Box component="span" sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}>
+  <Box component="span" sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}>
     •
   </Box>
 );
 
 export default function OutlinedCard() {
-  const {data:schoolCount} = useSchoolCount()
+  const { data: schoolCount } = useSchoolCount();
   return (
     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-      <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+      {/* <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
         <Card variant="outlined">
           <CardContent>
             <Typography variant="body2">Seasons Hosted</Typography>
@@ -25,8 +25,8 @@ export default function OutlinedCard() {
             </Typography>
           </CardContent>
         </Card>
-      </Grid>
-      <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+      </Grid> */}
+      {/* <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
         <Card variant="outlined">
           <CardContent>
             <Typography variant="body2">Total Contributions</Typography>
@@ -35,13 +35,13 @@ export default function OutlinedCard() {
             </Typography>
           </CardContent>
         </Card>
-      </Grid>
+      </Grid> */}
       <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
         <Card variant="outlined">
           <CardContent>
             <Typography variant="body2">Total School</Typography>
             <Typography variant="h5" component="div">
-              {schoolCount}
+              {schoolCount ? schoolCount : 'N/A'}
             </Typography>
           </CardContent>
         </Card>
