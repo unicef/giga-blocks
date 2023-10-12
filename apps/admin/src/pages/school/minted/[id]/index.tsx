6781884bdc25bc8650f23bcb83a0a631f2e24959
anupamakoirala <anukoirala.96@hotmail.com>
2023-10-12 15:23:22 +0545

    build issue fixes

--- apps/admin/src/pages/school/minted/[id]/index.tsx
+++ apps/admin/src/pages/school/minted/[id]/index.tsx
@@ -4,7 +4,6 @@ import { Grid } from '@mui/material';
 
 import DashboardLayout from '@layouts/dashboard';
 import SchoolDetails from './SchoolDetails';
-import GraphQlProvider from 'src/libs/graphql-query-client';
 
 
 UserEditPage.getLayout = (page: React.ReactElement) => (
@@ -24,9 +23,7 @@ export default function UserEditPage() {
     <title> School Details Page </title>
     </Head>
     <Grid container spacing={2}>
-    <GraphQlProvider>
     {id && <SchoolDetails id={id}/>}    
-    </GraphQlProvider>
     </Grid>
     </>
   );
