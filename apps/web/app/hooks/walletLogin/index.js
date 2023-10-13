a3edeb696de0ba3c005862d434eafb5b4194a599
sarvesh-karki <sarveshkarki93@gmail.com>
2023-10-13 12:01:44 +0545

    api and hooks to get nonce, register and login

---
+++ apps/web/app/hooks/walletLogin/index.js
@@ -0,0 +1,34 @@
+import {
+  BASE_URL,
+  NONCE,
+  WALLETREGISTER,
+  WALLETLOGIN,
+} from '../../constants/api';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const useGetNonce = () => {
+  return useQuery(async () => {
+    const nonce = await api.get(NONCE.GETNONCE);
+    return nonce;
+  });
+};
+
+export const walletRegister = () => {
+  const walletRegisterMutation = useMutation(async (payload) => {
+    const register = await api.post(WALLETREGISTER.WALLETREGISTER, payload);
+    return register;
+  });
+  return walletRegisterMutation;
+};
+export const walletLogin = () => {
+  const walletLoginMutation = useMutation(async (payload) => {
+    const register = await api.post(WALLETLOGIN.WALLETLOGIN, payload);
+    return register;
+  });
+  return walletLoginMutation;
+};
