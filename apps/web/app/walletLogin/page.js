7e970dbcbe1dae5c87229f7bdb6bc26523158fec
sarvesh-karki <sarveshkarki93@gmail.com>
2023-10-16 11:43:17 +0545

    new page for wallet login

--- apps/web/app/walletLogin/page.js
+++ apps/web/app/walletLogin/page.js
@@ -4,14 +4,12 @@ import Navbar from '../components/navbar';
 import { useForm, Controller } from 'react-hook-form';
 import { Button, Checkbox, Column, Form, Grid, TextInput } from '@carbon/react';
 import { Tile } from '@carbon/react';
-import './signup.scss';
+import './walletRegister.scss';
 import Link from 'next/link';
 import { useOtp } from '../hooks/useOtp';
 import { useSignUp } from '../hooks/useSignUp';
-import { useRouter } from 'next/navigation';
 
 const SignUp = () => {
-  const router = useRouter();
   const [email, setEmail] = useState('');
   const { handleSubmit, control } = useForm();
   const signUp = useSignUp();
@@ -27,17 +25,13 @@ const SignUp = () => {
     }
   };
 
-  const handlePageChange = () => {
-    router.push('/walletLogin');
-  };
-
   return (
     <>
       <Navbar />
       <Grid className="landing-page preview1Background signUp-grid" fullWidth>
         <Column className="form" md={4} lg={8} sm={4}>
           <Tile className="signUp-tile">
-            <h1>Create Your Account</h1>
+            <h1>Create Your Account Using Metamask</h1>
             <Form onSubmit={handleSubmit(onSubmit)}>
               <Controller
                 name="name"
@@ -59,41 +53,29 @@ const SignUp = () => {
                 )}
               />
               <Controller
-                name="email"
+                name="walletAddress"
                 control={control}
                 rules={{
-                  required: 'Email is required',
-                  pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                    message: 'Invalid email address',
-                  },
+                  required: '',
                 }}
                 render={({ field }) => (
                   <TextInput
                     {...field}
-                    id="email"
+                    id="walletAddress"
                     style={{ marginBottom: '25px', height: '48px' }}
-                    // invalid={!!errors.email}
-                    labelText="Email"
-                    placeholder="Enter your email here"
+                    labelText="Wallet Address"
+                    disabled
                   />
                 )}
               />
+              <br />
+              <Button className="submit-btn" type="submit">
+                Submit
+              </Button>
               <Checkbox
                 className="checkbox"
                 labelText="By creating an account, you agree to the Terms and conditions and our Privacy Policy"
               />
-              <br />
-              <Button
-                className="submit-btn"
-                type="submit"
-                style={{ marginRight: '14px' }}
-              >
-                Submit
-              </Button>
-              <Button className="submit-btn" onClick={handlePageChange}>
-                Login Using Metamask
-              </Button>
             </Form>
           </Tile>
           <p style={{ marginLeft: '20px' }}>
