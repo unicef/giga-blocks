fc9f0f337e95bb15718d39ee3751b4c3422b9855
sarvesh-karki <sarveshkarki93@gmail.com>
2023-10-17 21:26:11 +0545

    contribution form

--- apps/web/app/components/contribute/index.js
+++ apps/web/app/components/contribute/index.js
@@ -21,6 +21,21 @@ const ContributeForm = () => {
     { label: 'Choice B', value: 'choiceB' },
     { label: 'Choice C', value: 'choiceC' },
   ];
+  const dropdownOptions3 = [
+    { label: 'Choice A', value: 'choiceA' },
+    { label: 'Choice B', value: 'choiceB' },
+    { label: 'Choice C', value: 'choiceC' },
+  ];
+  const dropdownOptions4 = [
+    { label: 'Choice A', value: 'choiceA' },
+    { label: 'Choice B', value: 'choiceB' },
+    { label: 'Choice C', value: 'choiceC' },
+  ];
+  const dropdownOptions5 = [
+    { label: 'Choice A', value: 'choiceA' },
+    { label: 'Choice B', value: 'choiceB' },
+    { label: 'Choice C', value: 'choiceC' },
+  ];
 
   return (
     <>
@@ -32,33 +47,28 @@ const ContributeForm = () => {
         </Column>
         <Column md={4} lg={8} sm={4} className="school-detail-card">
           <Form onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-              name="name"
-              control={control} // Pass control to the Controller component
-              rules={{ required: 'Full Name is required' }}
-              render={({ field }) => (
-                <TextInput
-                  {...field}
-                  id="name"
-                  style={{ marginBottom: '25px', height: '48px' }}
-                  labelText="Full Name"
-                  placeholder="Enter your fullname here"
-                />
-              )}
+            <Dropdown
+              id="dropdown1"
+              titleText="Type of school"
+              label="Select options"
+              items={dropdownOptions1}
+              itemToString={(item) => (item ? item.label : '')}
+              selectedItem={null}
+              onChange={(selectedItem) => {
+                console.log('Selected Item 1:', selectedItem);
+              }}
             />
-            <Controller
-              name="address"
-              control={control} // Pass control to the Controller component
-              rules={{ required: 'Enter school address' }}
-              render={({ field }) => (
-                <TextInput
-                  {...field}
-                  id="address"
-                  style={{ marginBottom: '25px', height: '48px' }}
-                  labelText="Physical Address/ Nearest Settlement"
-                  placeholder="Enter school address"
-                />
-              )}
+            <Dropdown
+              id="dropdown2"
+              style={{ marginTop: '24px', marginBottom: '24px' }}
+              titleText="Country"
+              label="Select country"
+              items={dropdownOptions2}
+              itemToString={(item) => (item ? item.label : '')}
+              selectedItem={null}
+              onChange={(selectedItem) => {
+                console.log('Selected Item 2:', selectedItem);
+              }}
             />
             <Controller
               name="longlat"
@@ -82,146 +92,47 @@ const ContributeForm = () => {
                 </>
               )}
             />
-            <Controller
-              name="status"
-              control={control} // Pass control to the Controller component
-              rules={{ required: 'Enter school status' }}
-              render={({ field }) => (
-                <TextInput
-                  {...field}
-                  id="status"
-                  style={{ marginBottom: '25px', height: '48px' }}
-                  labelText="School Status"
-                  placeholder="Enter school status"
-                />
-              )}
-            />
-          </Form>
-        </Column>
-      </Grid>
-
-      {/* EDUCATION */}
-      <Grid fullWidth className="mt-50px">
-        <Column md={4} lg={5} sm={4}>
-          <span style={{ fontSize: '1.5em' }}>Education</span>
-        </Column>
-        <Column md={4} lg={8} sm={4} className="school-detail-card">
-          <Form onSubmit={handleSubmit(onSubmit)}>
             <Dropdown
-              id="dropdown1"
-              titleText="Select Option 1"
-              label="Select an option"
-              items={dropdownOptions1}
+              id="dropdown3"
+              style={{ marginBottom: '24px' }}
+              titleText="Connectivity"
+              label="Select connectivity"
+              items={dropdownOptions3}
               itemToString={(item) => (item ? item.label : '')}
-              selectedItem={null} // You can set the default selected item here if needed
+              selectedItem={null}
               onChange={(selectedItem) => {
-                // Handle dropdown item selection here
-                console.log('Selected Item 1:', selectedItem);
+                console.log('Selected Item 3:', selectedItem);
               }}
             />
             <Dropdown
-              id="dropdown2"
-              titleText="Select Option 2"
-              label="Select another option"
-              items={dropdownOptions2}
+              id="dropdown4"
+              style={{ marginTop: '24px', marginBottom: '24px' }}
+              titleText="Coverage Availability"
+              label="Select options"
+              items={dropdownOptions4}
               itemToString={(item) => (item ? item.label : '')}
-              selectedItem={null} // You can set the default selected item here if needed
+              selectedItem={null}
               onChange={(selectedItem) => {
-                // Handle dropdown item selection here
                 console.log('Selected Item 2:', selectedItem);
               }}
             />
-          </Form>
-        </Column>
-      </Grid>
-      {/* CONNECTIVITY */}
-      <Grid fullWidth className="mt-50px">
-        <Column md={4} lg={5} sm={4}>
-          <span style={{ fontSize: '1.5em' }}>Connectivity</span>
-        </Column>
-        <Column md={4} lg={8} sm={4} className="school-detail-card">
-          <Form onSubmit={handleSubmit(onSubmit)}>
-            <Controller
-              name="connectivityStatus"
-              control={control} // Pass control to the Controller component
-              render={({ field }) => (
-                <TextInput
-                  {...field}
-                  id="connectivityStatus"
-                  style={{ marginBottom: '25px', height: '48px' }}
-                  labelText="Connectivity Status"
-                  placeholder="Enter connectivity status here"
-                />
-              )}
+            <Dropdown
+              id="dropdown5"
+              style={{ marginTop: '24px', marginBottom: '24px' }}
+              titleText="Electricity Availability"
+              label="Select electricity availability"
+              items={dropdownOptions5}
+              itemToString={(item) => (item ? item.label : '')}
+              selectedItem={null}
+              onChange={(selectedItem) => {
+                console.log('Selected Item 2:', selectedItem);
+              }}
             />
-            <Controller
-              name="coverageAvailability"
-              control={control} // Pass control to the Controller component
-              rules={{ required: 'Enter school coverage availability' }}
-              render={({ field }) => (
-                <TextInput
-                  {...field}
-                  id="coverageAvailability"
-                  style={{ marginBottom: '25px', height: '48px' }}
-                  labelText="Coverage Availability"
-                  placeholder="Enter school coverage availability"
-                />
-              )}
-            />
-            <Controller
-              name="2gCovergage"
-              control={control} // Pass control to the Controller component
-              render={({ field }) => (
-                <>
-                  <TextInput
-                    {...field}
-                    id="2gCovergage"
-                    style={{ marginBottom: '25px', height: '48px' }}
-                    labelText="2G Coverage"
-                    placeholder="Enter 2G Coverage"
-                  />
-                </>
-              )}
-            />
-            <Controller
-              name="3gCoverage"
-              control={control} // Pass control to the Controller component
-              render={({ field }) => (
-                <>
-                  <TextInput
-                    {...field}
-                    id="3gCoverage"
-                    style={{ marginBottom: '25px', height: '48px' }}
-                    labelText="3G Coverage"
-                    placeholder="Enter 3G Coverage"
-                  />
-                </>
-              )}
-            />
-            <Controller
-              name="4gCoverage"
-              control={control} // Pass control to the Controller component
-              render={({ field }) => (
-                <>
-                  <TextInput
-                    {...field}
-                    id="4gCoverage"
-                    style={{ marginBottom: '25px', height: '48px' }}
-                    labelText="4G Coverage"
-                    placeholder="Enter 4G Coverage"
-                  />
-                </>
-              )}
-            />
-          </Form>
-          <Column md={4} lg={16} sm={4}>
-            <Button
-              type="submit"
-              style={{ width: '100%', marginBottom: '32px' }}
-            >
+
+            <Button style={{ width: '100%', marginBottom: '24px' }}>
               Submit
             </Button>
-          </Column>
+          </Form>
         </Column>
       </Grid>
     </>
