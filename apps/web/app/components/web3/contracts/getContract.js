33fe1a533294815866010a292a84d3dc8e0ab0d1
anupamakoirala <anukoirala.96@hotmail.com>
2023-10-19 13:44:44 +0545

    utility function to get contract added

--- apps/web/app/components/web3/contracts/getContract.js
+++ apps/web/app/components/web3/contracts/getContract.js
@@ -1,5 +1,6 @@
 import {useWeb3React} from '@web3-react/core';
 import { useMemo } from 'react';
+import Web3 from "web3"
 
 // export const getWeb3 = (chainId)=>{
 
@@ -10,15 +11,16 @@ export const useLibrary = ()=>{
         if(!chainId || !provider){
             return null;
         }
-        else return provider
+        else {
+            return new Web3(provider);
+        }
     },[chainId,provider])
 
 }
 
-export const getContract  =()=>{
+export const getContract  =(abi,address)=>{
     const library = useLibrary();
-
-    // if(!library  || !abi) return null;
+    if(!library  || !abi) return null;
     const contract =  new library.eth.Contract(abi,address);
     return contract;
 
