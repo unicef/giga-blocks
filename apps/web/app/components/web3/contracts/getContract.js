bd8fd7c6d635d2fec4c451c57822f8d93bfea50e
anupamakoirala <anukoirala.96@hotmail.com>
2023-10-31 15:53:17 +0545

    issue fixes in abi import

--- apps/web/app/components/web3/contracts/getContract.js
+++ apps/web/app/components/web3/contracts/getContract.js
@@ -2,7 +2,7 @@ import {useWeb3React} from '@web3-react/core';
 import { useMemo } from 'react';
 import Web3 from "web3"
 
-import GigaSeller from '../contracts/GigaSeller.json';
+import GigaSeller from '../../../constants/abi/GigaSeller.json';
 
 export const useLibrary = ()=>{
     const {provider,chainId} = useWeb3React();
