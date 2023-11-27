3b154d72c063334727dc51036e5c425786651964
Amrit Pandey <pandeyamrit714@gmail.com>
2023-11-27 12:13:43 +0545

    graph query updated

--- apps/web/app/libs/graph-query.js
+++ apps/web/app/libs/graph-query.js
@@ -1,41 +1,36 @@
+import { gql } from 'urql';
 
-import {gql} from "urql";
-
- const ownedNftsQuery = gql`
-
-   query  ownedNfts{ownedNfts(subgraphError: allow) {
+const ownedNftsQuery = gql`
+  query ownedNft($id: ID!, $first: Int!) {
+    ownedNft(id: $id, subgraphError: allow) {
       id
-      nfts {
+      nfts(first: $first) {
         id
         tokenUri
       }
     }
   }
-  `
- const nftListQuery = gql`
-
-  query tokenUris($first:Int!, ) {tokenUris(subgraphError: allow, first: $first) {
-    id
-    tokenUri
+`;
+const nftListQuery = gql`
+  query tokenUris($first: Int!) {
+    tokenUris(subgraphError: allow, first: $first) {
+      id
+      tokenUri
+    }
   }
-}`
+`;
 
 const nftDetailsQuery = gql`
-
-query tokenUri($id: ID!) {
-  tokenUri(id: $id, subgraphError: allow) {
-    id
-    tokenUri
+  query tokenUri($id: ID!) {
+    tokenUri(id: $id, subgraphError: allow) {
+      id
+      tokenUri
+    }
   }
-}
-`
+`;
 
-export const Queries ={
+export const Queries = {
   ownedNftsQuery,
   nftListQuery,
-  nftDetailsQuery
-}
-
-
-
- 
+  nftDetailsQuery,
+};
