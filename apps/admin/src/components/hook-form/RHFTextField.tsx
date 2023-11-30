1321eacd16b1e5031a51bb4c44c05ef8c8bb9bfc
Amrit Pandey <pandeyamrit714@gmail.com>
2023-11-30 20:35:48 +0545

    RHFTextField style modified

--- apps/admin/src/components/hook-form/RHFTextField.tsx
+++ apps/admin/src/components/hook-form/RHFTextField.tsx
@@ -18,31 +18,60 @@ export default function RHFTextField({ name, helperText, ...other }: Props) {
           fullWidth
           value={field.value}
           error={!!error}
+          // sx={{
+          //   '& .MuiOutlinedInput-root': {
+          //     '& fieldset': {
+          //       borderColor: 'default !important',
+          //       color: 'white !important',
+          //     },
+          //     '& .MuiOutlinedInput-notchedOutline': {
+          //       borderColor: 'rgba(145, 158, 171, 0.32) !important',
+          //     },
+          //     '& input': {
+          //       color: 'white !important', // Sets the input text color to white
+          //       boxShadow: '0 0 0 30px #005b5b inset !important',
+          //       caretColor: 'white !important', // Sets the input cursor blink color to white
+          //     },
+          //     '& input:-webkit-autofill': {
+          //       WebkitTextFillColor: 'white !important',
+          //     },
+          //   },
+          //   '& .MuiInputLabel-root, & .MuiInputLabel-root.Mui-focused': {
+          //     color: '#fff !important',
+          //   },
+          // }}
+          // FormHelperTextProps={{
+          //   style: {
+          //     color: '#f7931e', // Sets the helper text color to white
+          //   },
+          // }}
           sx={{
             '& .MuiOutlinedInput-root': {
               '& fieldset': {
-                borderColor: 'default !important',
-                color: 'white !important',
+                borderColor: '#efefef',
               },
-              '& .MuiOutlinedInput-notchedOutline': {
-                borderColor: 'rgba(145, 158, 171, 0.32) !important',
+              '&:hover fieldset': {
+                borderColor: '#efefef',
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: '#efefef',
               },
               '& input': {
-                color: 'white !important', // Sets the input text color to white
-                boxShadow: '0 0 0 30px #005b5b inset !important',
-                caretColor: 'white !important', // Sets the input cursor blink color to white
-              },
-              '& input:-webkit-autofill': {
-                WebkitTextFillColor: 'white !important',
+                color: 'white',
+                '&:-webkit-autofill': {
+                  WebkitBoxShadow: '0 0 0 100px #2C2B33 inset',
+                  WebkitTextFillColor: 'white !important',
+                },
               },
             },
-            '& .MuiInputLabel-root, & .MuiInputLabel-root.Mui-focused': {
-              color: '#fff !important',
+            '& .MuiInputLabel-root': {
+              color: '#efefef',
+              '&.Mui-focused': {
+                color: '#efefef',
+              },
             },
-          }}
-          FormHelperTextProps={{
-            style: {
-              color: '#f7931e', // Sets the helper text color to white
+            '& .MuiFormHelperText-root': {
+              color: 'red',
             },
           }}
           helperText={error ? error.message : helperText}
