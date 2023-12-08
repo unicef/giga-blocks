3f2a953099e8a857b1a46776d1345cc4fd0f3963
anupamakoirala <anukoirala.96@hotmail.com>
2023-12-08 11:12:05 +0545

    polygon network added during metaMask login

--- apps/web/app/components/web3/connectors/network.js
+++ apps/web/app/components/web3/connectors/network.js
@@ -4,12 +4,12 @@ import { Network } from '@web3-react/network';
 import { URLS } from '../chains';
 
  const DataUrl = {
-  // [80001]: 'https://api-testnet.polygonscan.com/api',
+  [80001]: 'https://rpc-mumbai.maticvigil.com',
   // [137]: 'https://api.polygonscan.com/api',
-  [421613]:'https://goerli-rollup.arbitrum.io/rpc',
+  // [421613]:'https://goerli-rollup.arbitrum.io/rpc',
 };
 
-export const Default_Chain_Id = 421613;
+export const Default_Chain_Id = 80001;
 
 
 export const [network, hooks] = initializeConnector(
