815fd59aa5cfca7a5b3c253f855e84471c517378
sarvesh-karki <sarveshkarki93@gmail.com>
2023-12-11 14:48:32 +0545

    toast in otp login

--- apps/web/app/components/modal/index.js
+++ apps/web/app/components/modal/index.js
@@ -1,4 +1,4 @@
-import { Modal, TextInput, Form, Button } from '@carbon/react';
+import { Modal, TextInput, Form, InlineNotification } from '@carbon/react';
 import { useLogin } from '../../hooks/useSignUp';
 import { useForm, Controller } from 'react-hook-form';
 import { saveAccessToken, saveCurrentUser } from '../../utils/sessionManager';
@@ -12,6 +12,7 @@ const CarbonModal = ({ open, onClose, email }) => {
   const { initialize } = useAuthContext();
   const login = useLogin();
   const { push } = useRouter();
+  const [notification, setNotification] = useState(null);
 
   const onAdd = async (data) => {
     const payload = {
@@ -24,45 +25,74 @@ const CarbonModal = ({ open, onClose, email }) => {
         saveCurrentUser(res.data);
         saveAccessToken(res.data.access_token);
         initialize();
+        setNotification({
+          kind: 'success',
+          title: 'OTP login successful.',
+        });
         push('/contributeSchool');
       })
       .catch((err) => {
         console.log(err);
+        setNotification({
+          kind: 'error',
+          title: 'Error in OTP login. Please try again.',
+        });
         setError('OTP verification unsuccessful. Please try again.');
       });
   };
 
+  const onCloseNotification = () => {
+    setNotification(null);
+  };
+
   return (
-    <Modal
-      open={open}
-      onSecondarySubmit={onClose}
-      modalHeading={!error ? `Please check your email` : error}
-      onRequestClose={onClose}
-      modalLabel={`OTP sent to ${email}.`}
-      secondaryButtonText="Cancel"
-      primaryButtonText="Submit"
-      onRequestSubmit={handleSubmit(onAdd)}
-    >
-      <Form>
-        <Controller
-          name="name"
-          control={control}
-          rules={{ required: 'Email is required' }}
-          render={({ field }) => (
-            <TextInput
-              {...field}
-              id="name"
-              // style={{height: "48px" }}
-              labelText="Enter OTP here"
-              placeholder=""
-              onChange={(e) => {
-                field.onChange(e);
-              }}
-            />
-          )}
+    <>
+      {notification && (
+        <InlineNotification
+          aria-label="closes notification"
+          kind={notification.kind}
+          onClose={onCloseNotification}
+          title={notification.title}
+          style={{
+            position: 'fixed',
+            top: '50px',
+            right: '2px',
+            width: '400px',
+            zIndex: 1000,
+          }}
         />
-      </Form>
-    </Modal>
+      )}
+      <Modal
+        open={open}
+        onSecondarySubmit={onClose}
+        modalHeading={!error ? `Please check your email` : error}
+        onRequestClose={onClose}
+        modalLabel={`OTP sent to ${email}.`}
+        secondaryButtonText="Cancel"
+        primaryButtonText="Submit"
+        onRequestSubmit={handleSubmit(onAdd)}
+      >
+        <Form>
+          <Controller
+            name="name"
+            control={control}
+            rules={{ required: 'Email is required' }}
+            render={({ field }) => (
+              <TextInput
+                {...field}
+                id="name"
+                // style={{height: "48px" }}
+                labelText="Enter OTP here"
+                placeholder=""
+                onChange={(e) => {
+                  field.onChange(e);
+                }}
+              />
+            )}
+          />
+        </Form>
+      </Modal>
+    </>
   );
 };
 
