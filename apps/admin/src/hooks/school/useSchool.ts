d32f1a207fd6fa0ca12f0e455458d4deb291cc51
anupamakoirala <anukoirala.96@hotmail.com>
2023-12-14 11:49:03 +0545

    fixes in admin site

--- apps/admin/src/hooks/school/useSchool.ts
+++ apps/admin/src/hooks/school/useSchool.ts
@@ -12,12 +12,12 @@ const accessToken = getAccessToken();
 
 api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 
-export const useSchoolGet = ({ page, perPage, minted, uploadId, name, country, connectivity }: { page?: number, perPage?: number, minted?: string, uploadId?: any, name?: string, country?: string, connectivity?: string }) => {
+export const useSchoolGet = ({page, perPage, minted, uploadId, name, country, connectivity}:{page?: number, perPage: number, minted?: string, uploadId?: any, name?: string, country?:string, connectivity?:string}) => {
   return useQuery(
     ['get-api-data', page, perPage],
     async () => {
       const { data } = await api.get(
-        `${routes.SCHOOLS.GET} ${perPage ? `?perPage=${perPage}` : ''}${page ? `&page=${page}` : ''}${name ? `&name=${name}` : ''}${minted ? `&minted=${minted}` : ''}${uploadId ? `&uploadId=${uploadId}` : ``}${country ? `&country=${country}` : ``}${connectivity ? `&connectivityStatus=${connectivity}` : ``}`
+        `${routes.SCHOOLS.GET}?perPage=${perPage}${page ? `&page=${page}` : ''}${name ? `&name=${name}` : ''}${minted ? `&minted=${minted}` : ''}${uploadId ? `&uploadId=${uploadId}` : ``}${country ? `&country=${country}` : ``}${connectivity ? `&connectivityStatus=${connectivity}` : ``}`
       );
       return data;
     },
@@ -26,6 +26,20 @@ export const useSchoolGet = ({ page, perPage, minted, uploadId, name, country, c
     }
   );
 };
+
+export const useAllSchool = () =>{
+  return useQuery(
+    ['get-all-school'],
+    async () =>{
+      const {data} = await api.get(
+        `${routes.SCHOOLS.GET}`
+      );
+      return data;
+    }
+  )
+  
+}
+
 
 export const useSchoolGetById = (id: string | undefined | string[]) => {
   return useQuery(
