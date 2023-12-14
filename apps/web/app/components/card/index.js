c642eff55281ce90a20af84b9f8a450c72ac2024
Raktim Shrestha <raktim@rumsan.com>
2023-12-15 02:23:39 +0545

    fixes for rendering p5 sketch

--- apps/web/app/components/card/index.js
+++ apps/web/app/components/card/index.js
@@ -11,22 +11,24 @@ import {
   Search,
 } from '@carbon/react';
 import './card.scss';
+import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 import { useQuery } from 'urql';
 import { toSvg } from 'jdenticon';
 import { Queries } from '../../libs/graph-query';
-import DynamicScript from './p5';
+
+const DynamicScript = dynamic(() => import('./p5'), { ssr: false });
 
 const SchoolCard = ({ query, variables, pageSize, setPageSize }) => {
   const [searchText, setSearchText] = useState('');
 
   const [result] = useQuery({
     query: query,
-    variables: {...variables,name:searchText},
+    variables: { ...variables, name: searchText },
   });
   const [imagedata] = useQuery({
-    query:Queries.nftImages ,
-    variables: {...variables},
+    query: Queries.nftImages,
+    variables: { ...variables },
   });
   const { data: queryData, fetching, error } = result;
   const [schoolData, setSchoolData] = useState([]);
@@ -41,26 +43,28 @@ const SchoolCard = ({ query, variables, pageSize, setPageSize }) => {
 
   useEffect(() => {
     if (queryData) decodeSchooldata(queryData);
-    if(imagedata) decodeImage(imagedata?.data?.nftImages)
-  }, [queryData]);
+    if (imagedata) decodeImage(imagedata?.data?.nftImages);
+  }, [queryData, imagedata]);
 
   const decodeImage = (data) => {
     const decodedImage = [];
     for (let i = 0; i < data?.length; i++) {
       const imageData = {
         tokenId: data[i].id,
-        image:  data[i].imageScript,
+        image: data[i].imageScript,
       };
       decodedImage.push(imageData);
     }
     setImageData(decodedImage);
-  }
+  };
 
   const decodeSchooldata = (data) => {
-    const encodeddata = variables?.id ? data?.collectorOwnedNft?.nfts : data?.nftDatas;
+    const encodeddata = variables?.id
+      ? data?.collectorOwnedNft?.nfts
+      : data?.nftDatas;
     const decodedShooldata = [];
 
-    if(!variables?.id){
+    if (!variables?.id) {
       for (let i = 0; i < encodeddata?.length; i++) {
         const schoolData = {
           tokenId: encodeddata[i].id,
@@ -69,16 +73,16 @@ const SchoolCard = ({ query, variables, pageSize, setPageSize }) => {
         };
         decodedShooldata.push(schoolData);
       }
-
+    } else {
+      for (let i = 0; i < encodeddata?.length; i++) {
+        const decodedData = atob(encodeddata[i].tokenUri.substring(29));
+        const schoolData = {
+          tokenId: encodeddata[i].id,
+          ...JSON.parse(decodedData),
+        };
+        decodedShooldata.push(schoolData);
+      }
     }
-   else  {for (let i = 0; i < encodeddata?.length; i++) {
-      const decodedData = atob(encodeddata[i].tokenUri.substring(29));
-      const schoolData = {
-        tokenId: encodeddata[i].id,
-        ...JSON.parse(decodedData),
-      };
-      decodedShooldata.push(schoolData);
-    }}
     setSchoolData(decodedShooldata);
   };
 
@@ -100,120 +104,122 @@ const SchoolCard = ({ query, variables, pageSize, setPageSize }) => {
         />
       </div>
       {fetching === false ? (
-
-      <Grid fullWidth style={{ margin: '30px auto' }}>
-        {schoolData.length > 0 ? (
-          schoolData?.map((school) => (
-            <Column sm={4}>
-              <ClickableTile
-                className="card"
-                href={`/explore/${school?.tokenId}`}
-              >
-                <div className="row">
-                  <DynamicScript scriptContent={imageData?.find((image)=>image.tokenId === school?.tokenId)?.image} />
-                  
-                  {/* <img
-                    src={generateIdenticon(school?.tokenId)}
-                    // src = {imageData?.find((image)=>image.tokenId === school?.tokenId)?.image}
-                    alt="SVG Image"
-                    style={{ marginBottom: '16px' }}
-                  /> */}
-                  {/* <p className="text-purple">School Name</p> */}
-                  <Toggletip align="right">
-                    <ToggletipButton label="Show information">
-                      <h4>
-                        {school?.schoolName?.length > 30
-                          ? `${school.schoolName
-                              ?.toLowerCase()
-                              .split(' ')
-                              .map(
-                                (word) =>
-                                  word.charAt(0).toUpperCase() + word.slice(1)
-                              )
-                              .join(' ')
-                              .slice(0, 30)}...`
-                          : school.schoolName
-                              ?.toLowerCase()
-                              .split(' ')
-                              .map(
-                                (word) =>
-                                  word.charAt(0).toUpperCase() + word.slice(1)
-                              )
-                              .join(' ')}
+        <Grid fullWidth style={{ margin: '30px auto' }}>
+          {schoolData.length > 0 ? (
+            schoolData.map((school, idx) => (
+              <Column key={idx} sm={4}>
+                <ClickableTile
+                  className="card"
+                  href={`/explore/${school?.tokenId}`}
+                >
+                  <div className="row">
+                    {imageData.length > 0 &&
+                    imageData?.find(
+                      (image) => image.tokenId === school?.tokenId
+                    )?.image ? (
+                      <DynamicScript
+                        scriptContent={
+                          imageData?.find(
+                            (image) => image.tokenId === school?.tokenId
+                          )?.image
+                        }
+                      />
+                    ) : null}
+                    {/* <p className="text-purple">School Name</p> */}
+                    <Toggletip align="right">
+                      <ToggletipButton label="Show information">
+                        <h4>
+                          {school?.schoolName?.length > 30
+                            ? `${school.schoolName
+                                ?.toLowerCase()
+                                .split(' ')
+                                .map(
+                                  (word) =>
+                                    word.charAt(0).toUpperCase() + word.slice(1)
+                                )
+                                .join(' ')
+                                .slice(0, 30)}...`
+                            : school.schoolName
+                                ?.toLowerCase()
+                                .split(' ')
+                                .map(
+                                  (word) =>
+                                    word.charAt(0).toUpperCase() + word.slice(1)
+                                )
+                                .join(' ')}
+                        </h4>
+                      </ToggletipButton>
+                      <ToggletipContent>
+                        <p>
+                          {school.schoolName
+                            ?.toLowerCase()
+                            .split(' ')
+                            .map(
+                              (word) =>
+                                word.charAt(0).toUpperCase() + word.slice(1)
+                            )
+                            .join(' ')}
+                        </p>
+                      </ToggletipContent>
+                    </Toggletip>
+                    <div>
+                      <h4 className="heading2 text-left">
+                        {school?.country
+                          ? school?.country?.length > 15
+                            ? `${school.country
+                                ?.toLowerCase()
+                                .split(' ')
+                                .map(
+                                  (word) =>
+                                    word.charAt(0).toUpperCase() + word.slice(1)
+                                )
+                                .join(' ')
+                                .slice(0, 15)}...`
+                            : school.country
+                                ?.toLowerCase()
+                                .split(' ')
+                                .map(
+                                  (word) =>
+                                    word.charAt(0).toUpperCase() + word.slice(1)
+                                )
+                                .join(' ')
+                          : 'N/A'}
                       </h4>
-                    </ToggletipButton>
-                    <ToggletipContent>
-                      <p>
-                        {school.schoolName
-                          ?.toLowerCase()
-                          .split(' ')
-                          .map(
-                            (word) =>
-                              word.charAt(0).toUpperCase() + word.slice(1)
-                          )
-                          .join(' ')}
-                      </p>
-                    </ToggletipContent>
-                  </Toggletip>
-                  <div>
-                    <h4 className="heading2 text-left">
-                      {school?.country
-                        ? school?.country?.length > 15
-                          ? `${school.country
-                              ?.toLowerCase()
-                              .split(' ')
-                              .map(
-                                (word) =>
-                                  word.charAt(0).toUpperCase() + word.slice(1)
-                              )
-                              .join(' ')
-                              .slice(0, 15)}...`
-                          : school.country
-                              ?.toLowerCase()
-                              .split(' ')
-                              .map(
-                                (word) =>
-                                  word.charAt(0).toUpperCase() + word.slice(1)
-                              )
-                              .join(' ')
-                        : 'N/A'}
-                    </h4>
-                  </div>
-                  {/* <p className="text-purple">Education Level</p> */}
-                  {/* <div>
+                    </div>
+                    {/* <p className="text-purple">Education Level</p> */}
+                    {/* <div>
                       <h4 className="heading2">
                         {school.covergeAvailability || 'N/A'}
                       </h4>
                     </div> */}
-                </div>
-              </ClickableTile>
+                  </div>
+                </ClickableTile>
+              </Column>
+            ))
+          ) : (
+            <Column sm={4} md={8} lg={16}>
+              <h1>No school has been minted</h1>
             </Column>
-          ))
-        ) : (
+          )}
           <Column sm={4} md={8} lg={16}>
-            <h1>No school has been minted</h1>
+            <Button
+              onClick={loadMore}
+              kind="tertiary"
+              disabled={allDataLoaded}
+              style={{ float: 'right' }}
+            >
+              {allDataLoaded === false ? 'Load more' : 'No more data'}
+            </Button>
           </Column>
-        )}
-        <Column sm={4} md={8} lg={16}>
-          <Button
-            onClick={loadMore}
-            kind="tertiary"
-            disabled={allDataLoaded}
-            style={{ float: 'right' }}
-          >
-            {allDataLoaded === false ? 'Load more' : 'No more data'}
-          </Button>
-        </Column>
-      </Grid>
-    
-  ) : (
-    <div className="loader-container">
-      <Loading withOverlay={false} />{' '}
-      <span>Loading school data, please wait...</span>{' '}
-    </div>
-  )}
-  </>
-  )
+        </Grid>
+      ) : (
+        <div className="loader-container">
+          <Loading withOverlay={false} />{' '}
+          <span>Loading school data, please wait...</span>{' '}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SchoolCard;
--- apps/web/app/components/card/p5.js
+++ apps/web/app/components/card/p5.js
@@ -1,63 +1,16 @@
-// import React, { useEffect } from 'react';
-
-// const DynamicSketch = ({ dynamicScript }) => {
-//   useEffect(() => {
-//     const loadScript = (src, callback) => {
-//       const script = document.createElement('script');
-//       script.src = src;
-//       script.onload = callback;
-//       document.body.appendChild(script);
-//     };
-
-//     const loadDynamicScripts = () => {
-//       // Load p5.js
-//       loadScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.min.js', () => {
-//         // Load dynamic script
-//         const sketchScript = document.createElement('script');
-//         sketchScript.innerHTML = dynamicScript;
-//         document.body.appendChild(sketchScript);
-//         console.log('Dynamic script added:', dynamicScript);
-//       });
-//     };
-
-//     // Call the function to load scripts
-//     loadDynamicScripts();
-//   }, [dynamicScript]);
-
-//   return <div id="dynamicSketchContainer"></div>;
-// };
-
-// export default DynamicSketch;
-
-import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 
 const DynamicSketch = ({ scriptContent }) => {
-    useEffect(() => {
-        console.log({ scriptContent })
-        if (scriptContent) {
-            // Load p5.js first
-            const p5Script = document.createElement('script');
-            p5Script.src = 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js';
-            document.body.appendChild(p5Script);
-
-            // Load p5.js first
-            const lodashScript = document.createElement('script');
-            lodashScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js';
-            document.body.appendChild(lodashScript);
-
-            // Load your custom script after p5.js is loaded
-            p5Script.onload = () => {
-                const script = document.createElement('script');
-                script.text = scriptContent;
-                document.body.appendChild(script);
-            };
-
-            return () => {
-                document.body.removeChild(p5Script);
-            };
-        }
-    }, [scriptContent]);
-
-    return <div id="p5-canvas">P5 Canvas</div>;
+  return (
+    <>
+      {scriptContent ? (
+        <Helmet>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min.js" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js" />
+          <script type="text/javascript">{scriptContent}</script>
+        </Helmet>
+      ) : null}
+    </>
+  );
 };
 export default DynamicSketch;
