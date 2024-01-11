ffcb2672fc3481964ff0f27d413aa74e605419e0
anupamakoirala <anukoirala.96@hotmail.com>
2024-01-11 12:32:55 +0545

    metamask signup issue fixes

--- apps/web/app/walletRegister/page.js
+++ apps/web/app/walletRegister/page.js
@@ -100,7 +100,7 @@ const WalletRegisterForm = () => {
       };
       registerMutation.mutateAsync(payload).then((res) => {
         saveCurrentUser(res.data.result);
-        saveAccessToken(res.data.access_token);
+        saveAccessToken(res.data.result.access_token);
         saveConnectors('metaMask');
         initialize();
         router.push('/dashboard');
