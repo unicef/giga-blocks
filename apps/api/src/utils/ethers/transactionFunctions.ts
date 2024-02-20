b0ebb114bb6e75883aed40f61f48e315c57847e2
anupamakoirala <anukoirala.96@hotmail.com>
2024-02-20 10:30:32 +0545

    docker file updated to fix puppeter issue.

--- apps/api/src/utils/ethers/transactionFunctions.ts
+++ apps/api/src/utils/ethers/transactionFunctions.ts
@@ -1,14 +1,21 @@
 import { BaseContract, ContractTransactionResponse } from 'ethers';
 import { getContractWithSigner, getInterface } from './contractWithSigner';
 import { ConfigService } from '@nestjs/config';
-import getProposedGasPrice from '../gasPrice';
+// import getProposedGasPrice from '../gasPrice';
 
 interface ExtendedContract extends BaseContract {
   // updateNftImageHash?: (schoolId: string, tokenHash: string, {gasPrice}?:{gasPrice: any}) => ContractTransactionResponse;
   updateNftImageHash?: any;
-  multicall?: (multicalldata, {gasPrice}:{gasPrice: bigint}) => ContractTransactionResponse;
+  multicall?: (
+    multicalldata,
+    // { gasPrice }: { gasPrice: bigint }
+  ) => ContractTransactionResponse;
   mintNft?: ([]) => ContractTransactionResponse;
-  updateNftContent?: (tokenId: string, schoolDataArray: any[], {gasPrice}:{gasPrice: bigint}) => ContractTransactionResponse;
+  updateNftContent?: (
+    tokenId: string,
+    schoolDataArray: any[],
+    // { gasPrice }: { gasPrice: bigint },
+  ) => ContractTransactionResponse;
   schoolIdToTokenId?: (
     schoolId: string | ContractTransactionResponse,
   ) => ContractTransactionResponse;
@@ -21,9 +28,9 @@ export const mintNFT = async (
   contractAddress: string,
   schoolDataArray: (string | boolean | number)[][],
   giga_ids: string[],
-) : Promise<ContractTransactionResponse> => {
+): Promise<ContractTransactionResponse> => {
   const config = new ConfigService();
-  const weiEthers = await getProposedGasPrice()
+  // const weiEthers = await getProposedGasPrice();
   const escrowAddress = config.get('NEXT_PUBLIC_GIGA_ESCROW_ADDRESS');
   const contract: ExtendedContract = getContractWithSigner(contractName, contractAddress);
   const schoolArgs = schoolDataArray.map((el, i) => [
@@ -33,7 +40,9 @@ export const mintNFT = async (
     el,
   ]);
   const multicalldata = generateMultiCallData(contractName, 'mintNft', schoolArgs);
-  return await contract.multicall(multicalldata, {gasPrice: weiEthers});
+  return await contract.multicall(multicalldata);
+
+  // return await contract.multicall(multicalldata, { gasPrice: weiEthers });
 };
 
 export const mintSingleNFT = async (
@@ -67,8 +76,10 @@ export const updateData = async (
   schoolDataArray: (string | boolean | number)[],
 ): Promise<ContractTransactionResponse> => {
   const contract: ExtendedContract = getContractWithSigner(contractName, contractAddress);
-  const weiEthers = await getProposedGasPrice()
-  return await contract.updateNftContent(tokenId, schoolDataArray, {gasPrice: weiEthers});
+  // const weiEthers = await getProposedGasPrice();
+  return await contract.updateNftContent(tokenId, schoolDataArray);
+
+  // return await contract.updateNftContent(tokenId, schoolDataArray, { gasPrice: weiEthers });
 };
 
 export const getTokenIdSchool = async (
@@ -95,9 +106,11 @@ export const updateImageHash = async (
   tokenHash: string,
   schoolId: string,
 ): Promise<ContractTransactionResponse> => {
-  const weiEthers = await getProposedGasPrice();
+  // const weiEthers = await getProposedGasPrice();
   const contract: ExtendedContract = getContractWithSigner(contractName, contractAddress);
-  return await contract.updateNftImageHash(schoolId, tokenHash, {gasPrice: weiEthers});
+  return await contract.updateNftImageHash(schoolId, tokenHash);
+
+  // return await contract.updateNftImageHash(schoolId, tokenHash, { gasPrice: weiEthers });
 };
 
 export const getTokenHash = async (
@@ -116,8 +129,9 @@ export const updateBulkData = async (
   schoolDataArray: (string | boolean | number)[][],
 ): Promise<ContractTransactionResponse> => {
   const contract: ExtendedContract = getContractWithSigner(contractName, contractAddress);
-  const weiEthers = await getProposedGasPrice()
+  // const weiEthers = await getProposedGasPrice();
   const schoolArgs = tokenId.map((el, i) => [el, schoolDataArray[i]]);
   const multicalldata = generateMultiCallData(contractName, 'updateNftContent', schoolArgs);
-  return await contract.multicall(multicalldata, {gasPrice: weiEthers});
+  return await contract.multicall(multicalldata);
+  // return await contract.multicall(multicalldata, { gasPrice: weiEthers });
 };
