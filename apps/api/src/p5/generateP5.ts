df3cff19ad609871bf5712ed65ed70a492f14e1d
anupama-rumsan <anupama@rumsan.net>
2024-12-18 14:28:48 +0545

    generate p5 function updated to address the new varaibles update

--- apps/api/src/p5/generateP5.ts
+++ apps/api/src/p5/generateP5.ts
@@ -1,16 +1,30 @@
 import { ContractTransactionResponse } from 'ethers';
 import puppeteer from 'puppeteer';
 
+interface scriptData {
+  tokenId: number;
+  nftcontents:any;
+  baseImage1: string;
+  baseImage2: string;
+}
+
 async function generateP5Image(
   p5Script: string | ContractTransactionResponse,
-  tokenId: number | ContractTransactionResponse,
+  scriptData: scriptData,
 ) {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     headless: true,
   });
+  //need to update the external variables
   const page = await browser.newPage();
-  const tokenNumber = Number(tokenId);
+  const tokenNumber = Number(scriptData.tokenId);
+  const baseImage1_value = scriptData.baseImage1;
+  const baseImage2_value = scriptData.baseImage2;
+  const connectivity_value = scriptData.nftcontents.connectivity;
+  const coverage_availability_value = scriptData.nftcontents.coverage_availabitlity;
+  const electricity_availability_value = scriptData.nftcontents.electricity_availability;
+
   const dataUrl = `
     <!DOCTYPE html>
     <html lang="en">
@@ -23,9 +37,12 @@ async function generateP5Image(
     <body>
       <div id="p5-container" style="display: block; background-color: red;"></div>
       <script>
-      let r = ${(tokenNumber * 54 + 12) % 255};
-      let g = ${(tokenNumber * 44 + 18) % 255};
-      let b = ${(tokenNumber * 74 + 10) % 255};
+      let baseImage1_value = "${baseImage1_value}";
+      let baseImage2_value = "${baseImage2_value}";
+      let tokenNumber = ${tokenNumber};
+      let connectivity_value = "${connectivity_value}";
+      let coverage_availability_value = "${coverage_availability_value}";
+      let electricity_availability_value = "${electricity_availability_value}";
         ${p5Script}
       </script>
     </body>
