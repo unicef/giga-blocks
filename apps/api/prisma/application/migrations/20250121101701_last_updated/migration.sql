e03dccaaf66a3c5a61ae514422207787e3d785b8
sushant-rumsan <sushant.tripathee@rumsan.net>
2025-01-29 14:55:48 +0545

    gist-and-hash-indexing

--- apps/api/prisma/application/migrations/20250121101701_last_updated/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `lastUpdated` on the `school_version` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "school_version" DROP COLUMN "lastUpdated",
-ADD COLUMN     "last_updated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
