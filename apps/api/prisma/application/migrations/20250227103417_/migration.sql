7092b84263abafd8f1b5d8f4475138ae7c217a5b
anupama-rumsan <anupama@rumsan.net>
2025-03-07 13:40:35 +0545

    migration file updated

--- apps/api/prisma/application/migrations/20250227103417_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `email` on table `giga_user_activation` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "giga_user_activation" ALTER COLUMN "email" SET NOT NULL;
--- apps/api/prisma/application/migrations/20250306110121_link/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "giga_school_activation" ADD COLUMN     "deActivatedBy" TEXT;
-
--- AddForeignKey
-ALTER TABLE "giga_school_activation" ADD CONSTRAINT "giga_school_activation_activatedBy_fkey" FOREIGN KEY ("activatedBy") REFERENCES "giga_users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "giga_school_activation" ADD CONSTRAINT "giga_school_activation_deActivatedBy_fkey" FOREIGN KEY ("deActivatedBy") REFERENCES "giga_users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
