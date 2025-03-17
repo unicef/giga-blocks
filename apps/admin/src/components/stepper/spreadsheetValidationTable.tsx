5db5ff1c62e62c5cec07884562723eff8360b12c
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2025-03-17 16:02:40 +0545

    upload school with region & redirect and msg update on successful upload

--- apps/admin/src/components/stepper/spreadsheetValidationTable.tsx
+++ apps/admin/src/components/stepper/spreadsheetValidationTable.tsx
@@ -17,7 +17,7 @@ import TableFormatter from '@utils/arrayFormatter';
 
 interface SpreadsheetValidationTableProps {
   setHasErrors: (hasErrors: boolean) => void;
-} 
+}
 
 const SpreadsheetValidationTable: React.FC<SpreadsheetValidationTableProps> = ({
   setHasErrors,
@@ -35,19 +35,17 @@ const SpreadsheetValidationTable: React.FC<SpreadsheetValidationTableProps> = ({
 
   const validateData = (data: Record<string, any>, fileType: string): string[] => {
     let hasIncorrectFileType = false;
-
     const hasCommaInName = data?.Name?.some((name: string) => name.includes(','));
-
     const validationErrors: string[] = [];
 
     if (hasIncorrectFileType) {
-      validationErrors?.push(
+      validationErrors.push(
         `Invalid Spreadsheet file uploaded. Please check correct file type and file name`
       );
     }
 
     if (hasCommaInName) {
-      validationErrors?.push('Name should not contain a comma');
+      validationErrors.push('Name should not contain a comma');
     }
 
     return validationErrors;
@@ -58,12 +56,11 @@ const SpreadsheetValidationTable: React.FC<SpreadsheetValidationTableProps> = ({
     const duplicateRows: any[] = [];
 
     data.forEach((row: any) => {
-      const rowString = JSON?.stringify(row);
-
+      const rowString = JSON.stringify(row);
       if (uniqueRows.has(rowString)) {
-        duplicateRows?.push(`Duplicate row found: ${JSON.stringify(row)} `);
+        duplicateRows.push(`Duplicate row found: ${JSON.stringify(row)}`);
       } else {
-        uniqueRows?.add(rowString);
+        uniqueRows.add(rowString);
       }
     });
 
@@ -74,7 +71,7 @@ const SpreadsheetValidationTable: React.FC<SpreadsheetValidationTableProps> = ({
     setSelectedSheetName(sheetName);
   };
 
-  const tableHeaders = convertedObject ? Object?.keys(convertedObject) : [];
+  const tableHeaders = convertedObject ? Object.keys(convertedObject) : [];
 
   useEffect(() => {
     const newAllSheetErrors: { sheetName: string; errors: string[] }[] = [];
@@ -82,28 +79,24 @@ const SpreadsheetValidationTable: React.FC<SpreadsheetValidationTableProps> = ({
       const duplicateErrors = duplicateCheck(allData);
       const formattedEachSheetData = TableFormatter(allData);
       const validationErrors = validateData(formattedEachSheetData, 'csv');
-      newAllSheetErrors?.push({
+      newAllSheetErrors.push({
         sheetName: sheetNames[0],
         errors: [...duplicateErrors, ...validationErrors],
       });
-      const hasErrors = newAllSheetErrors?.some((sheet) => sheet.errors.length > 0);
-      setHasErrors(hasErrors);
-
+      setHasErrors(newAllSheetErrors.some((sheet) => sheet.errors.length > 0));
       setAllSheetErrors(newAllSheetErrors);
     } else {
       allData.forEach((eachSheet: any, index: number) => {
         const duplicateErrors = duplicateCheck(eachSheet);
         const formattedEachSheetData = TableFormatter(eachSheet);
         const validationErrors = validateData(formattedEachSheetData, 'xls');
-        newAllSheetErrors?.push({
+        newAllSheetErrors.push({
           sheetName: sheetNames[index],
           errors: [...duplicateErrors, ...validationErrors],
         });
       });
 
-      const hasErrors = newAllSheetErrors?.some((sheet) => sheet.errors.length > 0);
-      setHasErrors(hasErrors);
-
+      setHasErrors(newAllSheetErrors.some((sheet) => sheet.errors.length > 0));
       setAllSheetErrors(newAllSheetErrors);
     }
   }, [allData, sheetNames, setHasErrors]);
@@ -112,7 +105,7 @@ const SpreadsheetValidationTable: React.FC<SpreadsheetValidationTableProps> = ({
     if (allSheetErrors) {
       let selectedSheetIndex = 0;
       if (selectedSheetName) {
-        selectedSheetIndex = allSheetErrors?.findIndex(
+        selectedSheetIndex = allSheetErrors.findIndex(
           (sheet) => sheet.sheetName === selectedSheetName
         );
       }
@@ -120,117 +113,55 @@ const SpreadsheetValidationTable: React.FC<SpreadsheetValidationTableProps> = ({
     }
   }, [allSheetErrors, selectedSheetName]);
 
-  const areArraysEqual = (array1:any, array2:any) => {
-    if (array1.length !== array2.length) {
-      return false;
-    }
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   useEffect(() => {
-    const hasDuplicates = (arry:string[]) => arry.filter((item, index) => arry.indexOf(item) !== index)
     const allowedElements = [
-      "schoolName",
-      "giga_school_id",
-      "longitudeStr",
-      "latitudeStr",
-      "schoolType",
-      "country ",
-      "connectivity",
-      "coverage_availabitlity",
-      "electricity_availability"
-  ]
-    if(tableHeaders.length > 0){
-      const isAnyMissing = allowedElements?.some(element => !tableHeaders?.includes(element));
-     
-      if(isAnyMissing === true){
-        const missingElements = allowedElements.filter(element => !tableHeaders.includes(element));
-        missingElements && setAllSheetErrors([{sheetName: 'school.csv', errors: [`${missingElements?.map((elem) => `${elem}`)} is missing, please follow sample file.`]}])
-        setHasErrors(true)
+      'schoolName',
+      'giga_school_id',
+      'longitudeStr',
+      'latitudeStr',
+      'schoolType',
+      'country',
+      'connectivity',
+      'coverage_availabitlity',
+      'electricity_availability',
+      'region',
+    ];
+
+    if (tableHeaders.length > 0) {
+      const missingElements = allowedElements.filter((element) => !tableHeaders.includes(element));
+
+      if (missingElements.length > 0) {
+        setAllSheetErrors([
+          {
+            sheetName: 'school.csv',
+            errors: [`${missingElements.join(', ')} is missing, please follow the sample file.`],
+          },
+        ]);
+        setHasErrors(true);
       }
-      
-      if(!areArraysEqual(allowedElements, tableHeaders)){
-        setAllSheetErrors([{sheetName: 'school.csv', errors: [`Header format did not match, please follow the sample file.`]}])
-        setHasErrors(true) 
-      }
-      
-      if(tableHeaders.length != 9){
-        setAllSheetErrors([{sheetName: 'school.csv', errors: [`Header format did not match, please follow the sample file.`]}])
-        setHasErrors(true) 
-      }
-      if(hasDuplicates(tableHeaders).length > 0) {
-        setAllSheetErrors([{sheetName: 'school.csv', errors: [`Duplicate columns, please follow sample file.`]}])
-        setHasErrors(true) 
-      }
-    } 
-  }, [convertedObject])
+    }
+  }, [convertedObject]);
 
   useEffect(() => {
     const updateConvertedObject = () => {
       const newConvertedObject = TableFormatter(rows);
       setConvertedObject(newConvertedObject);
     };
-
     updateConvertedObject();
   }, [rows]);
 
-  useEffect(() => {
-    if(convertedObject){
-    if(convertedObject?.schoolName?.length === 0){
-    setAllSheetErrors([{sheetName: 'school.csv', errors: ['Uplaoded csv is empty.']}]); 
-    setHasErrors(true)
-    }
-    if(Object.keys(convertedObject).length === 0){
-      setAllSheetErrors([{sheetName: 'school.csv', errors: ['Uplaoded csv is empty.']}]); 
-      setHasErrors(true)
-    }
-    }
-  }, [convertedObject])
-
-  let isInvalid:boolean = false;
-  useEffect(() => {
-    convertedObject &&
-    convertedObject[tableHeaders[0]]?.map((_: any, rowIndex: number) => (
-      tableHeaders?.map((header) => {
-        const value = convertedObject[header][rowIndex];
-        if (header === 'longitudeStr') {
-          isInvalid = isNaN(value) && isNaN(parseFloat(value));
-          if(isInvalid === true) {
-            setAllSheetErrors([{sheetName: 'school.csv', errors: ['Longitude must be a number.']}]); 
-            setHasErrors(true)
-          }
-        }
-
-        if (header === 'latitudeStr') {
-          isInvalid = isNaN(value) && isNaN(parseFloat(value))
-          if(isInvalid === true) {
-            setAllSheetErrors([{sheetName: 'school.csv', errors: ['Latitude must be a number.']}]); 
-            setHasErrors(true)
-          }
-        }
-      }
-      
-    )))
-  }, [convertedObject])
-
-
   return (
     <>
-      {errors?.length > 0 && (
+      {errors.length > 0 && (
         <Alert severity="error" sx={{ mb: 4, mx: 1 }}>
-          {errors?.map((error, index) => (
+          {errors.map((error, index) => (
             <div key={index}>- {error}</div>
           ))}
         </Alert>
       )}
 
-      {sheetNames?.length > 1 &&
-        sheetNames?.map((sheetName, index) => (
+      {sheetNames.length > 1 &&
+        sheetNames.map((sheetName, index) => (
           <Button
             variant="outlined"
             sx={{ mx: 1.5, my: 0.5 }}
@@ -245,7 +176,7 @@ const SpreadsheetValidationTable: React.FC<SpreadsheetValidationTableProps> = ({
         <Table sx={{ mx: 1 }}>
           <TableHead>
             <TableRow>
-              {tableHeaders?.map((header, index) => (
+              {tableHeaders.map((header, index) => (
                 <TableCell key={index} sx={{ whiteSpace: 'nowrap' }}>
                   {header}
                 </TableCell>
@@ -259,40 +190,16 @@ const SpreadsheetValidationTable: React.FC<SpreadsheetValidationTableProps> = ({
                   key={rowIndex}
                   sx={{ backgroundColor: rowIndex % 2 === 1 ? '#f5f5f5' : '#fff' }}
                 >
-                  {tableHeaders?.map((header) => {
+                  {tableHeaders.map((header) => {
                     const value = convertedObject[header][rowIndex];
                     let isInvalid = false;
 
-                    if (header === 'schoolName') {
-                      isInvalid = typeof(value) != 'string';
-                    }
-
-                    if (header === 'longitudeStr') {
+                    if (header === 'longitudeStr' || header === 'latitudeStr') {
                       isInvalid = isNaN(value) && isNaN(parseFloat(value));
-                    }
-
-                    if (header === 'latitudeStr') {
-                      isInvalid = isNaN(value) && isNaN(parseFloat(value))
-                    }
-
-                    if (header === 'schoolType') {
-                      isInvalid = typeof(value) != 'string';
-                    }
-
-                    if (header === 'country') {
-                      isInvalid = typeof(value) != 'string';
-                    }
-
-                    if (header === 'connectivity') {
-                      isInvalid = typeof(value) != 'string';
-                    }
-
-                    if (header === 'coverage_availabitlity') {
-                      isInvalid = typeof(value) != 'string';
-                    }
-
-                    if (header === 'electricity_availability') {
-                      isInvalid = typeof(value) != 'string';
+                    } else if (header === 'region') {
+                      isInvalid = typeof value !== 'string';
+                    } else {
+                      isInvalid = typeof value !== 'string';
                     }
 
                     const cellStyles = {
