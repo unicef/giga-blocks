2f38385cf81d028c2d1093a38da3e9ea8d6349cf
anupama-rumsan <anupama@rumsan.net>
2025-04-24 13:23:34 +0545

    contract hook updated to mint the nft

--- apps/giga/app/hooks/useContract/giga-contracts.ts
+++ apps/giga/app/hooks/useContract/giga-contracts.ts
@@ -1,31 +1,90 @@
+import { useMutation } from '@tanstack/react-query';
 import {
   useWriteGigaMinterBuyNft,
   useWriteGigaMinterMintNft,
 } from './gigaMinter';
 import { toast } from 'react-hot-toast';
+import { etherUnits, parseUnits } from 'viem';
 
 export const useGigaBuyNft = () => {
-  return useWriteGigaMinterBuyNft({
-    mutation: {
-      onSuccess: () => {
-        toast.success('NFT Purchase Success !!');
-      },
-      onError: () => {
-        toast.error('NFT Purchase Failed !!');
-      },
-    },
-  });
-};
+
+  const contract = useWriteGigaMinterBuyNft();
+
+  const functionCall = useMutation({
+    mutationFn:({
+      args,
+      totalValue,
+      contractAddress,
+    }:{
+      args:String,
+      totalValue: number,
+      contractAddress: string;
+    }) =>{
+      const weiValue = parseUnits(
+        totalValue.toString(),
+        etherUnits.wei,
+      )
+      // parseUnits(
+      //   totalValue.toString(),
+      //   18,
+      // );
+
+      return contract.writeContractAsync({
+        args: args,
+        contractAddress: contractAddress,
+        value: weiValue,
+      });
+    }
+      })
+      return functionCall
+    }
+
+
+  // return useWriteGigaMinterBuyNft({
+  //   mutation: {
+  //     onSuccess: () => {
+  //       toast.success('NFT Purchase Success !!');
+  //     },
+  //     onError: () => {
+  //       toast.error('NFT Purchase Failed !!');
+  //     },
+  //   },
+  // });
+
 
 export const useGigaMintNft = () => {
-  return useWriteGigaMinterMintNft({
-    mutation: {
-      onSuccess: () => {
-        toast.success('NFT Mint Successful !!');
-      },
-      onError: () => {
-        toast.error('NFT Mint Unsuccessful');
-      },
-    },
-  });
+  console.log('useGigaMintNft');
+  const contract = useWriteGigaMinterMintNft();
+  console.log('contract',contract);
+  const functionCall = useMutation({
+    mutationFn:({
+      id,
+      args,
+      contractAddress,
+
+    }:{
+      id: string;
+      args: any[];
+      contractAddress: string;
+    }) =>{
+      console.log('args', args);
+      return contract.writeContractAsync({
+        args: args,
+        contractAddress: contractAddress,
+      });
+    }
+      
+  // return useWriteGigaMinterMintNft({
+  //   mutation: {
+  //     onSuccess: () => {
+  //       toast.success('NFT Mint Successful !!');
+  //     },
+  //     onError: () => {
+  //       toast.error('NFT Mint Unsuccessful');
+  //     },
+  //   },
+  // });
+})
+return functionCall
+
 };
