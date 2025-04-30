b2c230ebbe740f325d39bee4597192d4734e9a0d
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2025-04-30 11:04:40 +0545

    check if wallet is connected on page load in dashboard

--- apps/giga/app/dashboard/page.jsx
+++ apps/giga/app/dashboard/page.jsx
@@ -6,10 +6,22 @@ import { userData } from './mockData';
 import { useQuery } from 'urql';
 import { Queries } from '../libs/graph-query';
 import { useAccount } from 'wagmi';
+import { Modal, Button } from '@carbon/react';
+import { ConnectKitButton } from 'connectkit';
 import SchoolCard from '../../components/schoolCard/SchoolCard';
+import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
-  const { address, isConnected } = useAccount();
+  const [showModal, setShowModal] = useState(false);
+  const { address, isConnected, isConnecting } = useAccount();
+
+  useEffect(() => {
+    if (!isConnecting && !isConnected) {
+      setShowModal(true);
+    } else if (isConnected) {
+      setShowModal(false);
+    }
+  }, [isConnected, isConnecting]);
 
   const [result] = useQuery({
     query: Queries.schoolOwnedNftsQuery,
@@ -29,91 +41,113 @@ export default function Dashboard() {
       }
     })
     .filter(Boolean);
-  console.log('decodedShooldata', decodedShooldata);
+  console.log('isConnecting', isConnecting);
 
   return (
-    <div className="dashboard-container">
-      <div className="dashboard-top-section">
-        <section className="profile-section">
-          <div className="profile-image-container">
-            <Image
-              src={userData.profileImage || '/placeholder.svg'}
-              alt="Profile avatar"
-              width={100}
-              height={100}
-              className="profile-image"
-            />
-          </div>
-          <h1 className="profile-name">
-            {address.slice(0, 4) + '...' + address.slice(35, 43)}
-          </h1>
-        </section>
+    <>
+      <Modal
+        open={showModal}
+        passiveModal
+        size="sm"
+        onRequestClose={() => setShowModal(false)}
+        modalHeading="Wallet not connected"
+      >
+        <p>To view your dashboard, please connect your wallet.</p>
+        <div style={{ marginTop: '30px' }}>
+          <ConnectKitButton />
+        </div>
+      </Modal>
+      <div className="dashboard-container">
+        <div className="dashboard-top-section">
+          <section className="profile-section">
+            <div className="profile-image-container">
+              <Image
+                src={userData.profileImage || '/placeholder.svg'}
+                alt="Profile avatar"
+                width={100}
+                height={100}
+                className="profile-image"
+              />
+            </div>
+            <h1 className="profile-name">
+              {isConnecting ? (
+                <p> Connecting...</p>
+              ) : !isConnected ? (
+                <p>Wallet not connected</p>
+              ) : (
+                address?.slice(0, 4) + '...' + address?.slice(35, 43)
+              )}
+            </h1>
+          </section>
 
-        {/* Latest School Reservation */}
-        <section className="latest-reservation">
-          <div className="reservation-card">
-            <div className="reservation-info">
-              <span className="reservation-label">
-                Latest School Reservation
-              </span>
-              {decodedShooldata?.length > 0 ? (
-                <>
-                  <h2 className="school-name">
-                    {decodedShooldata[0]?.schoolName}
-                  </h2>
-                  <p className="school-location">
-                    {decodedShooldata[0]?.region}
-                  </p>
-                </>
-              ) : (
-                <>
-                  <h2 className="school-name">Loading...</h2>
-                  <p className="school-location">Fetching latest reservation</p>
-                </>
-              )}
+          {/* Latest School Reservation */}
+          <section className="latest-reservation">
+            <div className="reservation-card">
+              <div className="reservation-info">
+                <span className="reservation-label">
+                  Latest School Reservation
+                </span>
+                {decodedShooldata?.length > 0 ? (
+                  <>
+                    <h2 className="school-name">
+                      {decodedShooldata[0]?.schoolName}
+                    </h2>
+                    <p className="school-location">
+                      {decodedShooldata[0]?.region}
+                    </p>
+                  </>
+                ) : (
+                  <>
+                    <h2 className="school-name">Loading...</h2>
+                    <p className="school-location">
+                      Fetching latest reservation
+                    </p>
+                  </>
+                )}
+              </div>
+              <div className="reservation-image-container">
+                {decodedShooldata?.length > 0 ? (
+                  <Image
+                    src={`https://ipfs.io/ipfs/${decodedShooldata[0]?.image}`}
+                    alt={`Image of ${decodedShooldata[0]?.schoolName}`}
+                    width={400}
+                    height={300}
+                    className="reservation-image"
+                  />
+                ) : (
+                  <Image
+                    src="/placeholder.svg"
+                    alt="Loading"
+                    width={300}
+                    height={200}
+                    className="reservation-image"
+                  />
+                )}
+              </div>
             </div>
-            <div className="reservation-image-container">
-              {decodedShooldata?.length > 0 ? (
-                <Image
-                  src={`https://ipfs.io/ipfs/${decodedShooldata[0]?.image}`}
-                  alt={`Image of ${decodedShooldata[0]?.schoolName}`}
-                  width={400}
-                  height={300}
-                  className="reservation-image"
+          </section>
+        </div>
+
+        {/* Reserved Schools Section */}
+        <section className="reserved-schools-section">
+          <h2 className="section-title">Reserved Schools</h2>
+
+          <div className="schools-grid">
+            {decodedShooldata?.slice(1, 5).map((school, index) => (
+              <div key={index} className="school-card">
+                <SchoolCard
+                  key={school.id}
+                  id={school.id}
+                  schoolName={school.schoolName}
+                  imageHash={school.image}
+                  location={school.region}
+                  minted={'MINTED'}
                 />
-              ) : (
-                <Image
-                  src="/placeholder.svg"
-                  alt="Loading"
-                  width={300}
-                  height={200}
-                  className="reservation-image"
-                />
-              )}
-            </div>
+              </div>
+            ))}
           </div>
         </section>
       </div>
-
-      {/* Reserved Schools Section */}
-      <section className="reserved-schools-section">
-        <h2 className="section-title">Reserved Schools</h2>
-
-        <div className="schools-grid">
-          {decodedShooldata?.slice(1, 5).map((school, index) => (
-            <div key={index} className="school-card">
-              <SchoolCard
-                key={school.id}
-                id={school.id}
-                schoolName={school.schoolName}
-                imageHash={school.image}
-                location={school.region}
-                minted={'MINTED'}
-              />
-            </div>
-          ))}
-        </div>
-      </section>
-    </div>
+    </>
   );
 }
