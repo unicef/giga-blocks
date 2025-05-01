6d5fa0aea8d574d117bfb8646db99ddda0e0a978
anupama-rumsan <anupama@rumsan.net>
2025-05-01 17:37:26 +0545

    schema designed for ciw

---
+++ apps/api/prisma/application/migrations/20250501115200_ciw/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "giga_ciw" (
+    "id" TEXT NOT NULL,
+    "did" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "giga_ciw_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "giga_ciw_did_key" ON "giga_ciw"("did");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "giga_ciw_email_key" ON "giga_ciw"("email");
