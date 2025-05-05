0883f7d493e6b6a9cce0c10829ae40a8043f8788
anupama-rumsan <anupama@rumsan.net>
2025-05-05 17:54:21 +0545

    locate on map redirection url updated

--- apps/giga/app/schools/[id]/page.jsx
+++ apps/giga/app/schools/[id]/page.jsx
@@ -101,8 +101,8 @@ export default function SchoolDetails({ params }) {
               name={data?.name}
               school_type={data?.school_type}
               region_name={data?.region_name}
-              longitude={data?.longitude}
-              latitude={data?.latitude}
+              locationId={data?.locationId}
+              countryCode={data?.countryCode}
               fontColor={fontColor}
             />
 
