17f8435d2b9af286ff20324b6381bc843f6566ab
anupama-rumsan <anupama@rumsan.net>
2025-05-12 18:17:15 +0545

    search feature integration in dashboard

--- apps/giga/components/landingBanner/landingBanner.jsx
+++ apps/giga/components/landingBanner/landingBanner.jsx
@@ -3,15 +3,27 @@
 import { Button, Column, Grid, Row, TextInput } from '@carbon/react';
 import { Search, ArrowRight } from '@carbon/icons-react';
 import './_landingBanner.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import CarbonButton from '../carbonButton';
+import { useRouter } from 'next/navigation';
 
 export default function LandingBanner() {
+  const [searchValue, setSearchValue] = useState('');
+  const router = useRouter();
   // return (
   //   <CarbonButton icon={ArrowRight} kind="primary">
   //     About GigaBlocks
   //   </CarbonButton>
   // );
+
+  const handleSearch = () => {
+    if (searchValue.trim()) {
+      router.push(`/schools?name=${encodeURIComponent(searchValue)}`);
+    } else {
+      //need to add a toast or alert
+      console.log('Please enter a school name to search.');
+    }
+  };
   return (
     <div className="hero-section">
       <Grid>
@@ -33,10 +45,13 @@ export default function LandingBanner() {
               labelText=""
               placeholder="Search School"
               className="search-input"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
             />
           </div>
           <div className="button-group">
             <Button
+              onClick={handleSearch}
               kind="primary"
               className="responsive-button"
               renderIcon={Search}
@@ -45,21 +60,25 @@ export default function LandingBanner() {
             </Button>
 
             <Button
+              href="/about"
               kind="secondary"
               renderIcon={ArrowRight}
               className="responsive-button"
             >
               About GigaBlocks
             </Button>
-            <CarbonButton icon={ArrowRight} kind="primary">
+            {/* <CarbonButton icon={ArrowRight} kind="primary">
               About GigaBlocks
-            </CarbonButton>
+            </CarbonButton> */}
           </div>
         </Column>
 
-        <Column lg={8} md={8} sm={4} className="grid-pattern-container">
-          
-        </Column>
+        <Column
+          lg={8}
+          md={8}
+          sm={4}
+          className="grid-pattern-container"
+        ></Column>
       </Grid>
     </div>
   );
