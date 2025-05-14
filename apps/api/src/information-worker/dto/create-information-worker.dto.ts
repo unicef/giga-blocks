d13e70fe9d407002cd5b3d797b60b6768b3f7697
anupama-rumsan <anupama@rumsan.net>
2025-05-14 11:28:02 +0545

    transform added in the dto to remove the whitespaces

--- apps/api/src/information-worker/dto/create-information-worker.dto.ts
+++ apps/api/src/information-worker/dto/create-information-worker.dto.ts
@@ -1,14 +1,17 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsOptional } from "class-validator";
+import { IsEmail, IsNotEmpty, IsOptional } from "class-validator";
+import { Transform } from "class-transformer";
 
 export class CreateInformationWorkerDto {
+    @Transform(({ value }) => value.trim())
     @ApiProperty({
         example:'Joe',
         description:'Name of the information worker'
     })
     @IsOptional()
     name?:string;
-
+    
+    @Transform(({ value }) => value.trim())
     @ApiProperty({
         example:'did:1234567890',
         description:'DID of the information worker'
@@ -16,10 +19,12 @@ export class CreateInformationWorkerDto {
     @IsNotEmpty()
     did:string;
 
+    @Transform(({ value }) => value.trim())
     @ApiProperty({
         example:'joe@gmail.com',
         description:'valid email of the information worker'
     })
+    @IsEmail()
     @IsNotEmpty()
     email:string;
 
