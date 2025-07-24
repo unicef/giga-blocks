42e0366c96c0176f105a1a13409fb6088ffa5565
anupamakoirala <anukoirala.96@hotmail.com>
2025-07-24 10:34:43 +0545

    mod: patch fixes reverted(stylus version issue)

--- apps/api/modifyPackage.ts
+++ apps/api/modifyPackage.ts
@@ -29,7 +29,6 @@ try {
     prisma: '^5.1.0',
     'ts-node': '^10.9.1',
     '@prisma/client': '^5.1.0',
-    'stylus': 'https://github.com/stylus/stylus.git#0.59.0' //need to remove the patch in later version
   };
 
   packageData.prisma = {
