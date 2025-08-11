d880bfed32704a34195285373f3f609832e7da85
anupamakoirala <anukoirala.96@hotmail.com>
2025-08-11 16:36:14 +0545

    api integration in  latest activated school section

--- apps/giga/components/schoolSearch/latestSchool/latestSchool.jsx
+++ apps/giga/components/schoolSearch/latestSchool/latestSchool.jsx
@@ -1,26 +1,50 @@
-import './_latestSchool.scss'
+'use client';
+import './_latestSchool.scss';
 import SchoolCard from '../../schoolCard/SchoolCard';
+import { useLatestActivatedSchool } from '../../../app/hooks/useSchool';
+import CardSkeleton from '../../cardSkeleton/CardSkeleton';
 
 export default function LatestSchool() {
+  const { data: schoolData, isLoading } = useLatestActivatedSchool();
   return (
     <section className="latest-schools">
       <div className="latest-schools__container">
-        <div className='latest-schools__header'>
-        <h2 className='latest-schools__title'>Latest Minted School</h2>
-        <p className='latest-schools__description'>
-          Go ahead, find a school and mint it's NFT. It's never been easier to
-          make a lasting impact{' '}
-        </p>
+        <div className="latest-schools__header">
+          <h2 className="latest-schools__title">Latest Minted School</h2>
+          <p className="latest-schools__description">
+            Go ahead, find a school and mint it's NFT. It's never been easier to
+            make a lasting impact{' '}
+          </p>
         </div>
-        <SchoolCard
-          key={1}
-          id={1}
-          schoolName={'name'}
-          location={'pohara'}
-          minted={'MINTED'}
-          hashImage={true}
-          imageHash={'dd'}
-        />
+        {isLoading ? (
+          <div className="latest-schools__grid">
+            <CardSkeleton count={5} />
+          </div>
+        ) : schoolData?.length > 0 ? (
+          <div className="latest-schools__grid">
+            {schoolData?.slice(0, 5).map((school) => (
+              <SchoolCard
+                key={school.id}
+                id={school.id}
+                schoolName={school.name}
+                location={school.country}
+                minted={'MINTED'}
+                hashImage={true}
+                imageHash={school.imageHash}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="featured-schools__no-data">
+            <p style={{ fontSize: '1.5rem', fontWeight: '400' }}>
+              There aren't any schools activated.
+            </p>
+            <p style={{ fontSize: '1.5rem', fontWeight: '400' }}>
+              {' '}
+              Be the initiator, Activate a school.
+            </p>
+          </div>
+        )}
       </div>
     </section>
   );
