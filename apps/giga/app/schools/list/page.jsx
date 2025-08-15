858d4d5b561d7fe61a020ac2128d3434eafe5824
anupamakoirala <anukoirala.96@hotmail.com>
2025-08-15 13:17:49 +0545

    loader screen added in case of expired link

--- apps/giga/app/schools/list/page.jsx
+++ apps/giga/app/schools/list/page.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import SchoolSearch from '../../../components/schoolSearch/schoolSearch/SchoolSearch';
 import { useGetActiveSchool } from '../../hooks/useActivation';
 import './_schoolList.scss';
+import CardSkeleton from '../../../components/cardSkeleton/CardSkeleton';
 
 const SchoolSearchPage = () => {
   const searchParams = useSearchParams();
@@ -29,34 +30,39 @@ const SchoolSearchPage = () => {
 
   return (
     <>
-      <SchoolSearch linkActivation={linkActivation} />
+      {/* <SchoolSearch linkActivation={linkActivation} /> */}
 
-      {showExpiredModal && (
-        <Modal
-          open={showExpiredModal}
-          passiveModal
-          size="sm"
-          className="dashboard-modal"
-          onRequestClose={handleModal}
-        >
-          <h3 className="dashboard-modal-activate-heading">
-            Oops! This event link is no longer active.
-          </h3>
-          <p className="dashboard-modal-activate-description">
-            Want to continue? 
-            <span onClick={() => (window.location.href = '/schools/list')}>
-              Browse the directory
-            </span>
-             or 
-            <span
-              onClick={() =>
-                (window.location.href = 'https://giga.global/contact-us/')
-              }
-            >
-              get in touch.
-            </span>
-          </p>
-        </Modal>
+      {showExpiredModal ? (
+        <>
+          <CardSkeleton count={4} />
+          <Modal
+            open={showExpiredModal}
+            passiveModal
+            size="sm"
+            className="dashboard-modal"
+            onRequestClose={handleModal}
+          >
+            <h3 className="dashboard-modal-activate-heading">
+              Oops! This event link is no longer active.
+            </h3>
+            <p className="dashboard-modal-activate-description">
+              Want to continue? 
+              <span onClick={() => (window.location.href = '/schools/list')}>
+                Browse the directory
+              </span>
+               or 
+              <span
+                onClick={() =>
+                  (window.location.href = 'https://giga.global/contact-us/')
+                }
+              >
+                get in touch.
+              </span>
+            </p>
+          </Modal>
+        </>
+      ) : (
+        <SchoolSearch linkActivation={linkActivation} />
       )}
     </>
   );
