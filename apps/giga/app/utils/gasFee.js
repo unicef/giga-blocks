8b61c17b8c73b914986d9ecf93a3fb6b10c98779
anupamakoirala <anukoirala.96@hotmail.com>
2025-09-26 12:58:15 +0545

    fixes: gas fee api updated due to version update

--- apps/giga/app/utils/gasFee.js
+++ apps/giga/app/utils/gasFee.js
@@ -3,8 +3,9 @@ import { ENDPOINTS } from '../constants/api';
 import { etherUnits, formatUnits } from 'viem';
 export const getGasPrice = async () => {
   const apiToken = process.env.NEXT_PUBLIC_GAS_API_KEY;
+  const chain = process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID;
   const data = await axios.get(
-    `${ENDPOINTS.GAS_FEE.GET}?module=proxy&action=eth_gasPrice&apikey=${apiToken}`
+    `${ENDPOINTS.GAS_FEE.GET}?chainid=${chain}&module=proxy&action=eth_gasPrice&apikey=${apiToken}`
   );
   const gasPriceHex = BigInt(data.data?.result * 2);
   const gasPriceInEth = formatUnits(gasPriceHex, 18);
